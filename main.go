5a62291dea96fe9625f6cc0b3aea2ab7518937e5
aiden <jonascarlsen77@gmail.com>
2021-04-14 00:43:43 +0000

    updated code

--- main.go
+++ main.go
@@ -1,41 +1,72 @@
-package main
+package siminn
 
 import (
-	"io/ioutil"
+	"context"
+	"errors"
+	"io"
 	"net/http"
-	"net/url"
+	"strconv"
+	"strings"
 )
 
+// SiminnSMS
+// ..
 type SiminnSMS struct {
-	URL      string
-	Username string
-	Password string
-	SendFrom string
+	URL      string // The URL for the sms service
+	Username string // A username you were given
+	Password string // A password you were given
+	SendFrom string // The name you want displayed as the sender
 }
 
-func (s *SiminnSMS) SendSMS(text string, number string) (error, bool) {
-	response, err := http.PostForm(s.URL+"push", url.Values{
-		"l":      {s.Username},
-		"p":      {s.Password},
-		"msisdn": {number},
-		"T":      {text},
-		"A":      {s.SendFrom},
-	},
-	)
-
+// SendSMS
+// This function send a text message using Siminn SMS api ( see docs in READMY )
+// At this moment it's a bit hacky because the API does not use HTTP Response
+// Codes in a proper manner, this will be updates as the service develops
+func (s *SiminnSMS) SendSMS(ctx context.Context, content string, number string) (error, bool, int) {
+	client := &http.Client{}
+	r, err := http.NewRequest(http.MethodPost, s.URL+"push", nil)
 	if err != nil {
-		return err, false
+		return err, false, 0
 	}
 
-	defer response.Body.Close()
-	_, err = ioutil.ReadAll(response.Body)
-	if err != nil {
-		return err, false
+	urlContsruction := r.URL.Query()
+	urlContsruction.Set("l", s.Username)
+	urlContsruction.Set("p", s.Password)
+	urlContsruction.Set("A", s.SendFrom)
+	urlContsruction.Set("T", content)
+	urlContsruction.Set("msisdn", number)
+	r.URL.RawQuery = urlContsruction.Encode()
+
+	r = r.WithContext(ctx)
+
+	response, sendError := client.Do(r)
+	if sendError != nil {
+		return sendError, false, 0
 	}
 
+	bodyBytes, responseReadErr := io.ReadAll(response.Body)
+	if responseReadErr != nil {
+		return responseReadErr, false, response.StatusCode
+	}
+
+	// super hack because it always returns 200
+	if strings.Contains(string(bodyBytes), "ERROR") {
+		responseError := errors.New(string(bodyBytes))
+		return responseError, false, response.StatusCode
+	}
+
+	// Again, hacky because of the way response are being made
+	if strings.Contains(string(bodyBytes), "SUCCESS") {
+		return nil, true, response.StatusCode
+	}
+
+	// A status code that is not 200, is not an intended error all we
+	// can do here is return the code and body for debugging
 	if response.StatusCode != 200 {
-		return nil, false
+		responseError := errors.New("Siminn returned a non 200 error:" + strconv.Itoa(response.StatusCode) + ": " + string(bodyBytes))
+		return responseError, false, response.StatusCode
 	}
 
-	return nil, true
+	// always default to fail
+	return nil, false, response.StatusCode
 }
--- main_test.go
+++ main_test.go
@@ -1,29 +1,32 @@
-package main
+package siminn
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
 func Test_single_sms(t *testing.T) {
 
+	ctx, _ := context.WithTimeout(context.Background(), 1500*time.Millisecond)
+
 	LoadEnv(".")
+
 	siminn := new(SiminnSMS)
 	siminn.Username = os.Getenv("USERNAME")
 	siminn.Password = os.Getenv("PASSWORD")
 	siminn.URL = "https://vasp.siminn.is/smap/"
 	siminn.SendFrom = os.Getenv("FROM")
-	err, ok := siminn.SendSMS("hello from siminn sms service", os.Getenv("NUMBER"))
-	if err != nil {
-		log.Println(err)
-		t.Fatal("Failed fetching token")
-	}
-	if !ok {
-		t.Fatal("Could not send text")
+
+	err, ok, code := siminn.SendSMS(ctx, "hello from siminn sms service", os.Getenv("NUMBER"))
+
+	if code != 200 || err != nil || !ok {
+		t.Fatal("Failed to send text", code, err, ok)
 	}
 }
 
